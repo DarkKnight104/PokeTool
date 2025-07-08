a0228248305827298118e21ff48d75c0473c58aa
Dark <tomnic18@gmail.com>
2025-07-08 20:44:23 +0200

    add csv parser + pokemon types

--- tools/pokemonparser.js
+++ tools/pokemonparser.js
@@ -2,11 +2,11 @@ const csv = require('csv-parser')
 const fs = require('fs')
 const results = [];
 
-fs.createReadStream('pokemon.csv')
+fs.createReadStream('tools/pokemon.csv')
   .pipe(csv())
   .on('data', (data) => results.push(data))
   .on('end', () => {
     console.log(results);
-    fs.writeFileSync('public/pokemon.json', JSON.stringify(results, null, 2));
+    fs.writeFileSync('docs/data/pokemon.json', JSON.stringify(results, null, 2));
     console.log("pokemon.json creato!");
   });
